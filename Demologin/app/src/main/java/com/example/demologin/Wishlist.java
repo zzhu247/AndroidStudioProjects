92fd71d2ff247aa33edde0eb1b7f03cd14558499
m272xu <m272xu@uwaterloo.ca>
2021-01-09 18:28:31 -0500

    implement drag and reorder

---
+++ Demologin/app/src/main/java/com/example/demologin/Wishlist.java
@@ -0,0 +1,87 @@
+package com.example.demologin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Wishlist extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    List<String> itemsList;
+
+    private FloatingActionButton eAddItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+
+        itemsList = new ArrayList<>();
+
+        // Display List
+        itemsList.add("Item 1");
+        itemsList.add("Item 2");
+        itemsList.add("Item 3");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(itemsList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        // Handle Add New Item Button
+        eAddItem = findViewById(R.id.addItem);
+
+        eAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to next page
+                Intent intent = new Intent(Wishlist.this, AddToWishlist.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    // Recycler view
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.START | ItemTouchHelper.END, 0) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+
+            int fromPosition = viewHolder.getAdapterPosition();
+            int toPosition = target.getAdapterPosition();
+            Collections.swap(itemsList, fromPosition, toPosition);
+            recyclerView.getAdapter().notifyItemMoved(fromPosition, toPosition);
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+        }
+    };
+}
