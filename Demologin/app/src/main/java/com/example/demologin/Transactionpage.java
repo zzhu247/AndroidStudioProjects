50dd88faa439d994151112a6ce73c11c4593369d
Anna Yu <annayu_10@hotmail.com>
2021-01-10 02:38:22 -0500

    created transaction page

---
+++ Demologin/app/src/main/java/com/example/demologin/Transactionpage.java
@@ -0,0 +1,14 @@
+package com.example.demologin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Transactionpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transactionpage);
+    }
+}
