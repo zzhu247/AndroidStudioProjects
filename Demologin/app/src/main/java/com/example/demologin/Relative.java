dbf7a47256d9e92ca87f8bdf51c54fb16afe92fc
Anna Yu <annayu_10@hotmail.com>
2021-01-10 05:26:02 -0500

    created the bottom-nav.
    USE THE TAB1, TAB2, TAB3 PAGES.
    
    instead of acitvitytranscation, activitywishlist, and activityhomepage

---
+++ Demologin/app/src/main/java/com/example/demologin/Relative.java
@@ -0,0 +1,52 @@
+package com.example.demologin;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class Relative extends AppCompatActivity {
+
+
+    ChipNavigationBar chipNavigationBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_relative);
+
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new tab1()).commit();
+        bottomMenu();
+
+    }
+
+    private void bottomMenu() {
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i) {
+                    case R.id.navigation_transaction:
+                        fragment = new ();
+                        break;
+
+                    case R.id.navigation_wishlist:
+                        fragment = new tab2();
+                        break;
+
+                    case R.id.navigation_profile:
+                        fragment = new tab3();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+    }
+}
